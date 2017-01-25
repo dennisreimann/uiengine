c416dba155bdf63668ff8fef87b73c0739e2e2db
Dennis Reimann <mail@dennisreimann.de>
2017-01-25 10:23:02 +0100

    ⚡️ rename the project to UIengine

--- packages/uiengine-core/lib/cli/commands/scaffold.js
+++ packages/uiengine-core/lib/cli/commands/scaffold.js
@@ -7,7 +7,7 @@ exports.builder = yargs =>
     .demandOption(['config'])
     .demandCommand(1)
     .describe('config', 'Path to config file')
-    .example('$0 scaffold <component_id> --config=hakuin.yml')
+    .example('$0 scaffold <component_id> --config=uiengine.yml')
 
 exports.handler = argv =>
   console.log('WIP')
