fd03e7e173a14da560ea9602a36df8a9d94d6869
Dennis Reimann <mail@dennisreimann.de>
2017-01-27 15:53:27 +0100

    💻 add basic cli init command

--- packages/uiengine-core/lib/cli/commands/init.js
+++ packages/uiengine-core/lib/cli/commands/init.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const File = require('../../util/file')
+
 exports.command = 'init [dir]'
 
 exports.desc = 'Create a basic structure and config file'
@@ -6,5 +9,55 @@ exports.builder = yargs =>
   yargs
     .default('dir', '.')
 
-exports.handler = argv =>
-  console.log('WIP')
+exports.handler = argv => {
+  const directory = path.resolve(process.cwd(), argv.dir)
+  const name = path.basename(directory)
+  const pagesDir = 'pages'
+  const configFileName = `${name}.yml`
+  const configContent = `
+name: ${name}
+
+target:
+  site: ./site
+  assets: ./site/assets
+
+basedirs:
+  components: ./src/components
+  templates: ./src/templates
+  includes: ./src/includes
+  pages: ./${pagesDir}
+`.trim()
+
+  const indexContent = `
+---
+title: Home
+---
+# Homepage
+
+Welcome!
+`.trim()
+
+  const configPath = path.relative(process.cwd(), path.join(directory, configFileName))
+  const indexPath = path.relative(process.cwd(), path.join(directory, pagesDir, `page.md`))
+  const createConfigFile = File.write(configPath, configContent)
+  const createIndexPage = File.write(indexPath, indexContent)
+
+  Promise.all([createConfigFile, createIndexPage])
+    .then((state) =>
+      console.log(`✅  ${name} initialized!
+
+The following files were created:
+
+- ${configPath} (uiengine configuration file)
+- ${indexPath} (index page)
+
+Build it using this command:
+
+$ uiengine site --config=${configFileName}
+
+Enjoy!`))
+    .catch((error) => {
+      console.error(`🚨  initializing ${name} failed! \n\n${error}`)
+      process.exit(1)
+    })
+}
