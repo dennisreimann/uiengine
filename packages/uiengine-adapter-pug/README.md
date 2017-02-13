25376702a9cdb9cae64f7a4fa509c28fd7c47e85
Dennis Reimann <mail@dennisreimann.de>
2017-02-13 21:33:40 +0100

    🎉 initial commit

---
+++ packages/uiengine-adapter-pug/README.md
@@ -0,0 +1,25 @@
+# UIengine template adapter for Pug
+
+## Configuration
+
+Plain and simple:
+
+```yaml
+adapters:
+  pug: uiengine-adapter-pug
+```
+
+With options:
+
+```yaml
+adapters:
+  pug:
+    module: uiengine-adapter-pug
+    options:
+      pretty: true
+      basedir: ./src/components
+```
+
+For available options see the [Pug options reference](https://pugjs.org/api/reference.html#options).
+
+
---
+++ packages/uiengine-adapter-pug/src/index.js
@@ -0,0 +1,23 @@
+const pug = require('pug')
+
+async function render (options, filePath, data = {}) {
+  return new Promise((resolve, reject) => {
+    const context = Object.assign(options, data)
+
+    try {
+      const rendered = pug.renderFile(filePath, context)
+
+      resolve(rendered)
+    } catch (err) {
+      reject([
+        `Pug could not render "${filePath}"!`,
+        err.stack,
+        JSON.stringify(context, null, '  ')
+      ].join('\n\n'))
+    }
+  })
+}
+
+module.exports = {
+  render
+}
