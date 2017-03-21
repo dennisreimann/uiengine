e707d6dd9c7210afe361a8c151365b3bc51d5118
Dennis Reimann <mail@dennisreimann.de>
2017-03-21 16:20:34 +0100

    🕶 alias shortnames for cli commands

--- packages/uiengine-core/src/cli/index.js
+++ packages/uiengine-core/src/cli/index.js
@@ -7,15 +7,18 @@ require('yargs')
   .global('debug')
   .default('debug', false)
   .describe('debug', 'Flag to enable debug mode')
+  .alias('d', 'debug')
   // config
   .string('config')
   .global('config')
   .default('config', UIengine.CONFIG_FILENAME)
   .describe('config', 'Path to config file')
+  .alias('c', 'config')
   // command
   .demandCommand(1)
   .commandDir(path.join(__dirname, 'commands'))
   // help / usage
   .usage(`Usage: $0 <command> [options]`)
   .help()
+  .alias('h', 'help')
   .argv
