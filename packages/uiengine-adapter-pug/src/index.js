a4029a9ba904f6bcd0edf30f4022d51cf1e473be
Dennis Reimann <mail@dennisreimann.de>
2017-05-10 20:36:53 +0200

    Add include and attributes

--- packages/uiengine-adapter-pug/src/index.js
+++ packages/uiengine-adapter-pug/src/index.js
@@ -21,7 +21,7 @@ const filesForComponent = (componentName) =>
   [
     {
       basename: `${componentName}.pug`,
-      data: `mixin ${componentName}()\n  .${componentName}\n    //- TODO: implement\n`
+      data: `mixin ${componentName}()\n  .${componentName}&attributes(attributes)\n    //- TODO: implement\n`
     }
   ]
 
@@ -29,7 +29,7 @@ const filesForVariant = (componentName, variantName) =>
   [
     {
       basename: `${variantName}.pug`,
-      data: `+${componentName}()\n`
+      data: `include ../${componentName}.pug\n\n+${componentName}()\n`
     }
   ]
 
