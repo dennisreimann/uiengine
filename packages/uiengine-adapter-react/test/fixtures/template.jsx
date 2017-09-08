baa4d26950a741c0bec0ef4c69ffdfaa9f1a0d1c
Dennis Reimann <mail@dennisreimann.de>
2017-09-08 11:26:29 +0200

    🚦 add basic react adapter test

---
+++ packages/uiengine-adapter-react/test/fixtures/template.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Template (props) {
+  return (
+    <p>
+      {props.myData}
+    </p>
+  )
+}
---
+++ packages/uiengine-adapter-react/test/react_adapter_test.js
@@ -0,0 +1,21 @@
+const assert = require('assert')
+const path = require('path')
+const Adapter = require('../src/index')
+
+describe('React adapter', () => {
+  describe('#render', () => {
+    it('should render the template with the given data', done => {
+      const templatePath = path.resolve(__dirname, 'fixtures', 'template.jsx')
+      const options = {}
+      const data = { myData: 1 }
+
+      Adapter.render(options, templatePath, data)
+        .then(rendered => {
+          assert(rendered.match(/<p data-react(.*?)>1<\/p>/))
+
+          done()
+        })
+        .catch(done)
+    })
+  })
+})
