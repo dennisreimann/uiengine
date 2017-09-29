7ef827aee8da960a5ca9969c763fe3287b4a0ed1
Dennis Reimann <mail@dennisreimann.de>
2017-09-29 20:46:41 +0200

    🛣 dynamic theme path resolving

--- packages/uiengine/src/integrations/common.js
+++ packages/uiengine/src/integrations/common.js
@@ -2,15 +2,20 @@ const path = require('path')
 const UIengine = require('../uiengine')
 const debounce = require('../util/debounce')
 
-const sourceFilesFromConfig = ({ source: { configFile, components, data, schema, pages, templates }, adapters, debug }) => {
+const sourceFilesFromConfig = ({ source: { configFile, components, data, schema, pages, templates }, adapters, debug, theme }) => {
   const exts = '.{' + Object.keys(adapters).concat('md').join(',') + '}'
   const componentsGlob = components ? path.join(components, '**/*' + exts) : null
   const templatesGlob = templates ? path.join(templates, '**/*' + exts) : null
   const pagesGlob = templates ? path.join(pages, '**') : null
   const dataGlob = data ? path.join(data, '**') : null
   const schemaGlob = data ? path.join(schema, '**/*.yml') : null
-  const themeGlob = debug ? path.join(process.cwd(), 'node_modules/uiengine-theme-default/{lib,static}', '**') : null
-  const sourceFiles = [configFile, componentsGlob, dataGlob, schemaGlob, pagesGlob, templatesGlob, themeGlob].filter(a => a)
+  const sourceFiles = [configFile, componentsGlob, dataGlob, schemaGlob, pagesGlob, templatesGlob].filter(a => a)
+
+  if (debug) {
+    const themeLibGlob = path.join(path.dirname(require.resolve(theme.module)), '**')
+    const themeStaticGlob = path.join(require(theme.module).staticPath, '**')
+    sourceFiles.push(themeLibGlob, themeStaticGlob)
+  }
 
   return sourceFiles
 }
