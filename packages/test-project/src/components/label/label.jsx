b924c31a8ee40d8e670b801abccf7e9a314ae73b
Dennis Reimann <mail@dennisreimann.de>
2017-10-19 12:59:19 +0200

    🆙 upgrade dependencies

--- packages/test-project/src/components/label/label.jsx
+++ packages/test-project/src/components/label/label.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Label ({ children, ...props }) {
   return (
-    <label className='label' for={props.for}>
+    <label className='label' htmlFor={props.for}>
       {props.title}
     </label>
   )
