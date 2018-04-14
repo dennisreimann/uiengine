e130a80be120054a6ebc498e9d5841944fc690a7
Dennis Reimann <mail@dennisreimann.de>
2018-04-14 12:07:54 +0200

    🤙 improve handlebars adapter test coverage

--- packages/adapter-handlebars/src/index.js
+++ packages/adapter-handlebars/src/index.js
@@ -2,11 +2,23 @@ const { readFile } = require('fs')
 const { basename, extname } = require('path')
 const Handlebars = require('handlebars')
 
-async function registerComponentFile (options, filePath) {
+const handleError = (options, err, reject, filePath, raw, data) => {
+  const message = [`Handlebars could not handle "${filePath}"!`, err]
+
+  if (options.debug) message.push(raw, JSON.stringify(data, null, 2))
+
+  const error = new Error(message.join('\n\n'))
+  error.code = err.code
+  error.path = filePath
+
+  reject(error)
+}
+
+export async function registerComponentFile (options, filePath) {
   return new Promise((resolve, reject) => {
     readFile(filePath, 'utf8', (err, raw) => {
       if (err) {
-        reject(err)
+        handleError(options, err, reject, filePath, raw)
       } else {
         const name = basename(filePath, extname(filePath))
         const id = options.namespace ? `${options.namespace}/${name}` : name
@@ -19,11 +31,11 @@ async function registerComponentFile (options, filePath) {
   })
 }
 
-async function render (options, filePath, data = {}) {
+export async function render (options, filePath, data = {}) {
   return new Promise((resolve, reject) => {
     readFile(filePath, 'utf8', (err, raw) => {
       if (err) {
-        reject(err)
+        handleError(options, err, reject, filePath, raw, data)
       } else {
         try {
           const template = Handlebars.compile(raw)
@@ -31,18 +43,9 @@ async function render (options, filePath, data = {}) {
 
           resolve(rendered)
         } catch (err) {
-          const message = [`Handlebars could not render "${filePath}"!`, err]
-
-          if (options.debug) message.push(raw, JSON.stringify(data, null, 2))
-
-          reject(message.join('\n\n'))
+          handleError(options, err, reject, filePath, raw, data)
         }
       }
     })
   })
-}
-
-module.exports = {
-  registerComponentFile,
-  render
 }
--- packages/adapter-handlebars/test/handlebars_adapter_test.js
+++ packages/adapter-handlebars/test/handlebars_adapter_test.js
@@ -1,16 +1,76 @@
 const assert = require('assert')
+const { assertMatches } = require('../../../test/support/asserts')
 const { resolve } = require('path')
+const Handlebars = require('handlebars')
 const Adapter = require('../src/index')
+
+const options = {}
+const data = { myData: 1 }
+const templatePath = resolve(__dirname, 'fixtures', 'template.hbs')
 
 describe('Handlebars adapter', () => {
   describe('#render', () => {
     it('should render the template with the given data', async () => {
-      const templatePath = resolve(__dirname, 'fixtures', 'template.hbs')
-      const options = {}
-      const data = { myData: 1 }
       const rendered = await Adapter.render(options, templatePath, data)
 
       assert.equal(rendered, '<p>1</p>\n')
+    })
+
+    it('should throw error if the file does not exist', async () => {
+      try {
+        await Adapter.render(options, 'does-not-exist.hbs')
+      } catch (error) {
+        assert(error)
+
+        assertMatches(error.message, 'Handlebars could not handle "does-not-exist.hbs"')
+      }
+    })
+
+    it('should throw detailed error if the debug option is set', async () => {
+      try {
+        const templatePath = resolve(__dirname, 'fixtures', 'invalid-template.hbs')
+        await Adapter.render({ debug: true }, templatePath, data)
+      } catch (error) {
+        assert(error)
+
+        assertMatches(error.message, '<p>{{myData')
+        assertMatches(error.message, '"myData": 1')
+      }
+    })
+  })
+
+  describe('#registerComponentFile', () => {
+    it('should register the template as partial', async () => {
+      await Adapter.registerComponentFile(options, templatePath)
+
+      assert.equal(Handlebars.partials['template'], '<p>{{myData}}</p>\n')
+    })
+
+    it('should register the template as namespaced partial', async () => {
+      await Adapter.registerComponentFile({ namespace: 'adapter' }, templatePath)
+
+      assert.equal(Handlebars.partials['adapter/template'], '<p>{{myData}}</p>\n')
+    })
+
+    it('should throw error if the file does not exist', async () => {
+      try {
+        await Adapter.registerComponentFile(options, 'does-not-exist.hbs')
+      } catch (error) {
+        assert(error)
+
+        assertMatches(error.message, 'Handlebars could not handle "does-not-exist.hbs"')
+      }
+    })
+
+    it('should throw detailed error if the debug option is set', async () => {
+      try {
+        const templatePath = resolve(__dirname, 'fixtures', 'invalid-template.hbs')
+        await Adapter.registerComponentFile({ debug: true }, templatePath)
+      } catch (error) {
+        assert(error)
+
+        assertMatches(error.message, '<p>{{myData')
+      }
     })
   })
 })
