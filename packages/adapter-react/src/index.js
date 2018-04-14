82fdc0e24496b3271c14917f496b5bd024f2a912
Dennis Reimann <mail@dennisreimann.de>
2018-04-14 13:29:21 +0200

    🤙 improve react adapter test coverage

--- packages/adapter-react/src/index.js
+++ packages/adapter-react/src/index.js
@@ -5,14 +5,16 @@ require('babel-register')({})
 // invalidate require cache so we get template updates as well
 const invalidateModuleCache = filePath => delete require.cache[require.resolve(filePath)]
 
+const upcaseFirstChar = string => string.charAt(0).toUpperCase() + string.slice(1)
+
 export async function registerComponentFile (options, filePath) {
   invalidateModuleCache(filePath)
 }
 
 export async function render (options, filePath, data = {}) {
-  invalidateModuleCache(filePath)
-
   try {
+    invalidateModuleCache(filePath)
+
     let template = require(filePath)
     if (template.default) template = template.default
     const vdom = template(data)
@@ -25,4 +27,44 @@ export async function render (options, filePath, data = {}) {
 
     throw new Error(message.join('\n\n'))
   }
+}
+
+export function filesForComponent (componentName) {
+  const name = upcaseFirstChar(componentName)
+
+  return [
+    {
+      basename: `${name}.jsx`,
+      data: `import React from 'react'
+
+const ${name} = props => {
+  return (
+    <div className='${componentName}'>
+      {props.children}
+    </div>
+  )
+}
+
+export default ${name}
+`
+    }
+  ]
+}
+
+export function filesForVariant (componentName, variantName) {
+  const cName = upcaseFirstChar(componentName)
+  const vName = upcaseFirstChar(variantName)
+
+  return [
+    {
+      basename: `${vName}.jsx`,
+      data: `import React from 'react'
+import ${cName} from '../${cName}.jsx'
+
+export default props => (
+  <${cName} />
+)
+`
+    }
+  ]
 }
--- packages/adapter-react/test/react_adapter_test.js
+++ packages/adapter-react/test/react_adapter_test.js
@@ -1,16 +1,70 @@
 const assert = require('assert')
+const { assertMatches } = require('../../../test/support/asserts')
 const { resolve } = require('path')
 const Adapter = require('../src/index')
+
+const templatePath = resolve(__dirname, 'fixtures', 'template.jsx')
 
 describe('React adapter', () => {
   describe('#render', () => {
     it('should render the template with the given data', async () => {
-      const templatePath = resolve(__dirname, 'fixtures', 'template.jsx')
       const options = {}
       const data = { myData: 1 }
       const rendered = await Adapter.render(options, templatePath, data)
 
       assert(rendered.match(/<p data-react(.*?)>1<\/p>/))
+    })
+
+    it('should throw error if the file does not exist', async () => {
+      try {
+        await Adapter.render({}, 'does-not-exist.jsx')
+      } catch (error) {
+        assert(error)
+
+        assertMatches(error.message, 'React DOM could not render "does-not-exist.jsx"')
+      }
+    })
+
+    it('should throw detailed error if the debug option is set', async () => {
+      try {
+        const templatePath = resolve(__dirname, 'fixtures', 'invalid-template.jsx')
+        const data = { myData: 1 }
+        await Adapter.render({ debug: true }, templatePath, data)
+      } catch (error) {
+        assert(error)
+
+        assertMatches(error.message, 'invalid-template.jsx')
+        assertMatches(error.message, '"myData": 1')
+      }
+    })
+  })
+
+  describe('#registerComponentFile', () => {
+    it('should invalidate the module cache', async () => {
+      // is set by previous render test
+      assert(require.cache[require.resolve(templatePath)])
+
+      await Adapter.registerComponentFile({}, templatePath)
+
+      assert(!require.cache[require.resolve(templatePath)])
+    })
+  })
+
+  describe('#filesForComponent', () => {
+    it('should return the component file', () => {
+      const files = Adapter.filesForComponent('button')
+
+      assert.equal(files.length, 1)
+      assert.equal(files[0].basename, 'Button.jsx')
+    })
+  })
+
+  describe('#filesForVariant', () => {
+    it('should return the variant file', () => {
+      const files = Adapter.filesForVariant('button', 'primary')
+
+      assert.equal(files.length, 1)
+      assert.equal(files[0].basename, 'Primary.jsx')
     })
   })
 })
