e546e43792ebedc933a5c3d23180cb1422dfa66a
Michael Geers <michael.geers@neuland-bfi.de>
2018-05-03 11:49:31 +0200

    restored debounce delay

--- packages/core/src/util/debounce.js
+++ packages/core/src/util/debounce.js
@@ -1,6 +1,6 @@
 const timers = {}
 
-export const debounce = (key, fn, delay = 100) => {
+export const debounce = (key, fn, delay = 250) => {
   clearTimeout(timers[key])
   timers[key] = setTimeout(fn, delay)
 }
