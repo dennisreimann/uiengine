3825339740fe7b51b081d19b97e81a284e0e7278
Michael Geers <michael.geers@neuland-bfi.de>
2018-05-03 11:22:42 +0200

    reduced 2s file watching delay for faster refreshes

--- packages/core/src/util/debounce.js
+++ packages/core/src/util/debounce.js
@@ -1,6 +1,6 @@
 const timers = {}
 
-export const debounce = (key, fn, delay = 250) => {
+export const debounce = (key, fn, delay = 100) => {
   clearTimeout(timers[key])
   timers[key] = setTimeout(fn, delay)
 }
