764b7729186cc4dbb266c10673979349dd1143f1
Dennis Reimann <mail@dennisreimann.de>
2018-06-17 11:12:29 +0200

    👍 improve internal link handling

--- packages/ui/src/vue/components/ContentText.vue
+++ packages/ui/src/vue/components/ContentText.vue
@@ -31,11 +31,24 @@ export default {
       const { target } = $event
 
       if (target && target.matches(".content a:not([href*='://'])") && target.href) {
+        // some sanity checks taken from vue-router:
+        // https://github.com/vuejs/vue-router/blob/dev/src/components/link.js#L106
+        const { altKey, ctrlKey, metaKey, shiftKey, button, defaultPrevented } = $event
+        // don't handle with control keys
+        if (metaKey || altKey || ctrlKey || shiftKey) return
+        // don't handle when preventDefault called
+        if (defaultPrevented) return
+        // don't handle right clicks
+        if (button !== undefined && button !== 0) return
+        // don't handle if `target="_blank"`
+        if (target && target.getAttribute) {
+          const linkTarget = target.getAttribute('target')
+          if (/\b_blank\b/i.test(linkTarget)) return
+        }
+        // don't handle same page links/anchors
         const url = new URL(target.href)
         const to = url.pathname
-
-        // do not handle same page links/anchors
-        if (window.location.pathname !== to) {
+        if (window.location.pathname !== to && $event.preventDefault) {
           $event.preventDefault()
           this.$router.push(to)
         }
