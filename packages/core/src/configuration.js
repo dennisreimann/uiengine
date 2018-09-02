c821b6ba6b72c6f2715743dd40c2ca7a8ae0ce49
Dennis Reimann <mail@dennisreimann.de>
2018-09-02 10:59:58 +0200

    🐛 check for presence of source.components before resolving

--- packages/core/src/configuration.js
+++ packages/core/src/configuration.js
@@ -103,7 +103,10 @@ const _read = (configFilePath, projectConfig, flags) => {
   source = R.map(resolvePaths, source)
   source.base = resolve(configPath)
   source.configFile = resolvePath(configPath, configFilePath)
-  source.components = resolvePathAsArray(configPath, source.components)
+  if (source.components) {
+    source.components = resolvePathAsArray(configPath, source.components)
+  }
+
   target = resolvePath(configPath, target)
   adapters = R.map(resolveAdapters, adapters || {})
   ui = data.ui || {}
