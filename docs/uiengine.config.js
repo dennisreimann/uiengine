9e5585fef212c0c0f7984f9f108f6846d3defca9
Dennis Reimann <mail@dennisreimann.de>
2019-04-05 12:37:39 +0200

    🏎 ui: add resource hints and improve performance

--- docs/uiengine.config.js
+++ docs/uiengine.config.js
@@ -1,5 +1,13 @@
 const pkg = require('../packages/core/package.json')
 
+const preconnect = href => ({
+  tag: 'link',
+  attrs: {
+    rel: 'preconnect',
+    href
+  }
+})
+
 module.exports = {
   name: 'UIengine Documentation',
   version: pkg.version,
@@ -11,7 +19,15 @@ module.exports = {
   target: 'dist',
 
   ui: {
-    customStylesFile: '/styles/docs.css'
+    customStylesFile: '/styles/docs.css',
+    meta: [
+      preconnect('https://s.ytimg.com'),
+      preconnect('https://i.ytimg.com'),
+      preconnect('https://www.google.com'),
+      preconnect('https://fonts.gstatic.com'),
+      preconnect('https://www.youtube-nocookie.com'),
+      preconnect('https://yt3.ggpht.com')
+    ]
   },
 
   browserSync: {
