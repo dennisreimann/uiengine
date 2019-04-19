8eea4e4f7cd56047f57d57c6b4e5318e7c2425cd
Matteo Fogli <matteo@modo.md>
2019-04-19 18:23:39 +0200

    fix inversion of logic on multithemes

--- packages/ui/src/vue/components/MainComponent.vue
+++ packages/ui/src/vue/components/MainComponent.vue
@@ -335,7 +335,7 @@ export default {
     },
 
     displayVariant (variant) {
-      return !!(!variant.theme || this.currentTheme.id.includes(variant.theme))
+      return !!(!variant.theme || variant.theme.includes(this.currentTheme.id))
     },
 
     tabId (section) {
