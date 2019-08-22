55926135b67b4798477a2bbd6fac094d7bf07656
Dennis Reimann <mail@dennisreimann.de>
2019-08-22 11:41:28 +0200

    🔎 docs: add algolia powered fulltext search
    Signed-off-by: Dennis Reimann <mail@dennisreimann.de>

--- docs/uiengine.config.js
+++ docs/uiengine.config.js
@@ -13,7 +13,8 @@ module.exports = {
   version: pkg.version,
 
   source: {
-    pages: 'site'
+    pages: 'site',
+    additionalWatches: ['./*.md', './adapter/*.md']
   },
 
   target: 'dist',
@@ -28,13 +29,37 @@ module.exports = {
           content: 'The UIengine is a workbench for UI-driven development: A tool for developers and designers to build and document web sites and apps.'
         }
       },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css'
+        }
+      },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'prefetch',
+          href: 'https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js'
+        }
+      },
+      preconnect('https://cdn.jsdelivr.net'),
       preconnect('https://s.ytimg.com'),
       preconnect('https://i.ytimg.com'),
       preconnect('https://www.google.com'),
       preconnect('https://fonts.gstatic.com'),
       preconnect('https://www.youtube-nocookie.com'),
       preconnect('https://yt3.ggpht.com')
-    ]
+    ],
+    foot: `
+      <script src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
+      <script>
+        docsearch({
+          apiKey: '9feb71b81a25c046ff57fe54b86f879b',
+          indexName: 'uiengine',
+          inputSelector: '.topbar input[name="query"]'
+        });
+      </script>`
   },
 
   browserSync: {
